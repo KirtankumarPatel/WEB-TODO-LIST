ac380eafc1b337d5acf3e29b587b8b179eb021a8
BhargavPatel <kirtan9157@gmail.com>
2023-08-29 23:27:33 +0530

    Table Modal added

---
+++ AspCore-MVC-Todo/Models/Entities/Todo.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspCore_MVC_Todo.Models.Entities
+{
+
+    // Todo table entity modal 
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(250)]
+        [Required]
+        public string Task { get; set; }
+        public bool IsCompleted { get; set; } = false;
+
+        [Display(Name = "CompleteOn")]
+        [DataType(DataType.Date)]
+        public DateTime? CompleteOn { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Created { get; set; } = DateTime.Now;
+
+    }
+}
---
+++ AspCore-MVC-Todo/Models/TodoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AspCore_MVC_Todo.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace AspCore_MVC_Todo.Models
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todo { get; set; }
+    }
+}
