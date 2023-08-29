b5c0d9ef6ba6947fc50396ab1a7c327065ed8f99
BhargavPatel <kirtan9157@gmail.com>
2023-08-29 23:16:49 +0530

    Created MVC project

---
+++ AspCore-MVC-Todo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspCore_MVC_Todo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
